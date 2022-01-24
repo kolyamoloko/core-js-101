806fc3d0ae65a494f2ccf33ba0e21d2ae232225a
kolyamoloko <kolyamoloko@mail.ru>
2022-01-24 21:47:34 +0300

    add: 48

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Returns the rectangle object with width and height parameters and getArea() method
  *
@@ -20,10 +19,13 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = function () {
+    return width * height;
+  };
 }
-
 
 /**
  * Returns the JSON representation of specified object
